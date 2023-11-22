72381307dd3c2ac76473223ac3dab14e16a1fe10
Creeeeger <bauaasgd@gmail.com>
2023-11-22 18:37:56 +0000

    fix2

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-/* This file is auto generated, version 19 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#19 SMP PREEMPT Wed Nov 22 11:45:10 UTC 2023"
-=======
 /* This file is auto generated, version 25 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
 #define UTS_VERSION "#25 SMP PREEMPT Wed Nov 22 16:46:26 UTC 2023"
->>>>>>> 74c0715a1a25 (fix_issues_and_start_of_mod_gpu)
 #define LINUX_COMPILE_BY "parallels"
 #define LINUX_COMPILE_HOST "ubuntu-linux-22-04-desktop"
 #define LINUX_COMPILER "gcc version 11.4.0 (Ubuntu 11.4.0-1ubuntu1~22.04)"
