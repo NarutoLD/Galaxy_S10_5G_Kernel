d254dbd0006693096f2067c796511a435bee0d13
Creeeeger <bauaasgd@gmail.com>
2024-03-26 21:11:45 +0900

    Add erofs drivers

--- include/asm-generic/barrier.h
+++ include/asm-generic/barrier.h
@@ -20,7 +20,7 @@
 #include <linux/compiler.h>
 
 #ifndef nop
-#define nop()	asm volatile ("nop")
+#define nop()    asm volatile ("nop")
 #endif
 
 /*
@@ -31,111 +31,111 @@
  */
 
 #ifndef mb
-#define mb()	barrier()
+#define mb()    barrier()
 #endif
 
 #ifndef rmb
-#define rmb()	mb()
+#define rmb()    mb()
 #endif
 
 #ifndef wmb
-#define wmb()	mb()
+#define wmb()    mb()
 #endif
 
 #ifndef dma_rmb
-#define dma_rmb()	rmb()
+#define dma_rmb()    rmb()
 #endif
 
 #ifndef dma_wmb
-#define dma_wmb()	wmb()
+#define dma_wmb()    wmb()
 #endif
 
 #ifndef read_barrier_depends
-#define read_barrier_depends()		do { } while (0)
+#define read_barrier_depends()        do { } while (0)
 #endif
 
 #ifndef __smp_mb
-#define __smp_mb()	mb()
+#define __smp_mb()    mb()
 #endif
 
 #ifndef __smp_rmb
-#define __smp_rmb()	rmb()
+#define __smp_rmb()    rmb()
 #endif
 
 #ifndef __smp_wmb
-#define __smp_wmb()	wmb()
+#define __smp_wmb()    wmb()
 #endif
 
 #ifndef __smp_read_barrier_depends
-#define __smp_read_barrier_depends()	read_barrier_depends()
+#define __smp_read_barrier_depends()    read_barrier_depends()
 #endif
 
 #ifdef CONFIG_SMP
 
 #ifndef smp_mb
-#define smp_mb()	__smp_mb()
+#define smp_mb()    __smp_mb()
 #endif
 
 #ifndef smp_rmb
-#define smp_rmb()	__smp_rmb()
+#define smp_rmb()    __smp_rmb()
 #endif
 
 #ifndef smp_wmb
-#define smp_wmb()	__smp_wmb()
+#define smp_wmb()    __smp_wmb()
 #endif
 
 #ifndef smp_read_barrier_depends
-#define smp_read_barrier_depends()	__smp_read_barrier_depends()
+#define smp_read_barrier_depends()    __smp_read_barrier_depends()
 #endif
 
-#else	/* !CONFIG_SMP */
+#else    /* !CONFIG_SMP */
 
 #ifndef smp_mb
-#define smp_mb()	barrier()
+#define smp_mb()    barrier()
 #endif
 
 #ifndef smp_rmb
-#define smp_rmb()	barrier()
+#define smp_rmb()    barrier()
 #endif
 
 #ifndef smp_wmb
-#define smp_wmb()	barrier()
+#define smp_wmb()    barrier()
 #endif
 
 #ifndef smp_read_barrier_depends
-#define smp_read_barrier_depends()	do { } while (0)
+#define smp_read_barrier_depends()    do { } while (0)
 #endif
 
-#endif	/* CONFIG_SMP */
+#endif    /* CONFIG_SMP */
 
 #ifndef __smp_store_mb
 #define __smp_store_mb(var, value)  do { WRITE_ONCE(var, value); __smp_mb(); } while (0)
 #endif
 
 #ifndef __smp_mb__before_atomic
-#define __smp_mb__before_atomic()	__smp_mb()
+#define __smp_mb__before_atomic()    __smp_mb()
 #endif
 
 #ifndef __smp_mb__after_atomic
-#define __smp_mb__after_atomic()	__smp_mb()
+#define __smp_mb__after_atomic()    __smp_mb()
 #endif
 
 #ifndef __smp_store_release
-#define __smp_store_release(p, v)					\
-do {									\
-	compiletime_assert_atomic_type(*p);				\
-	__smp_mb();							\
-	WRITE_ONCE(*p, v);						\
+#define __smp_store_release(p, v)                    \
+do {                                    \
+    compiletime_assert_atomic_type(*p);                \
+    __smp_mb();                            \
+    WRITE_ONCE(*p, v);                        \
 } while (0)
 #endif
 
 #ifndef __smp_load_acquire
-#define __smp_load_acquire(p)						\
-({									\
-	typeof(*p) ___p1 = READ_ONCE(*p);				\
-	compiletime_assert_atomic_type(*p);				\
-	__smp_mb();							\
-	___p1;								\
+#define __smp_load_acquire(p)                        \
+({                                    \
+    typeof(*p) ___p1 = READ_ONCE(*p);                \
+    compiletime_assert_atomic_type(*p);                \
+    __smp_mb();                            \
+    ___p1;                                \
 })
 #endif
 
@@ -146,11 +146,11 @@ do {									\
 #endif
 
 #ifndef smp_mb__before_atomic
-#define smp_mb__before_atomic()	__smp_mb__before_atomic()
+#define smp_mb__before_atomic()    __smp_mb__before_atomic()
 #endif
 
 #ifndef smp_mb__after_atomic
-#define smp_mb__after_atomic()	__smp_mb__after_atomic()
+#define smp_mb__after_atomic()    __smp_mb__after_atomic()
 #endif
 
 #ifndef smp_store_release
@@ -161,40 +161,40 @@ do {									\
 #define smp_load_acquire(p) __smp_load_acquire(p)
 #endif
 
-#else	/* !CONFIG_SMP */
+#else    /* !CONFIG_SMP */
 
 #ifndef smp_store_mb
 #define smp_store_mb(var, value)  do { WRITE_ONCE(var, value); barrier(); } while (0)
 #endif
 
 #ifndef smp_mb__before_atomic
-#define smp_mb__before_atomic()	barrier()
+#define smp_mb__before_atomic()    barrier()
 #endif
 
 #ifndef smp_mb__after_atomic
-#define smp_mb__after_atomic()	barrier()
+#define smp_mb__after_atomic()    barrier()
 #endif
 
 #ifndef smp_store_release
-#define smp_store_release(p, v)						\
-do {									\
-	compiletime_assert_atomic_type(*p);				\
-	barrier();							\
-	WRITE_ONCE(*p, v);						\
+#define smp_store_release(p, v)                        \
+do {                                    \
+    compiletime_assert_atomic_type(*p);                \
+    barrier();                            \
+    WRITE_ONCE(*p, v);                        \
 } while (0)
 #endif
 
 #ifndef smp_load_acquire
-#define smp_load_acquire(p)						\
-({									\
-	typeof(*p) ___p1 = READ_ONCE(*p);				\
-	compiletime_assert_atomic_type(*p);				\
-	barrier();							\
-	___p1;								\
+#define smp_load_acquire(p)                        \
+({                                    \
+    typeof(*p) ___p1 = READ_ONCE(*p);                \
+    compiletime_assert_atomic_type(*p);                \
+    barrier();                            \
+    ___p1;                                \
 })
 #endif
 
-#endif	/* CONFIG_SMP */
+#endif    /* CONFIG_SMP */
 
 /* Barriers for virtual machine guests when talking to an SMP host */
 #define virt_mb() __smp_mb()
@@ -203,7 +203,7 @@ do {									\
 #define virt_read_barrier_depends() __smp_read_barrier_depends()
 #define virt_store_mb(var, value) __smp_store_mb(var, value)
 #define virt_mb__before_atomic() __smp_mb__before_atomic()
-#define virt_mb__after_atomic()	__smp_mb__after_atomic()
+#define virt_mb__after_atomic()    __smp_mb__after_atomic()
 #define virt_store_release(p, v) __smp_store_release(p, v)
 #define virt_load_acquire(p) __smp_load_acquire(p)
 
@@ -217,7 +217,31 @@ do {									\
  * Architectures that do not do load speculation can have this be barrier().
  */
 #ifndef smp_acquire__after_ctrl_dep
-#define smp_acquire__after_ctrl_dep()		smp_rmb()
+#define smp_acquire__after_ctrl_dep()        smp_rmb()
+#endif
+
+/**
+ * smp_cond_load_relaxed() - (Spin) wait for cond with no ordering guarantees
+ * @ptr: pointer to the variable to wait on
+ * @cond: boolean expression to wait for
+ *
+ * Equivalent to using READ_ONCE() on the condition variable.
+ *
+ * Due to C lacking lambda expressions we load the value of *ptr into a
+ * pre-named variable @VAL to be used in @cond.
+ */
+#ifndef smp_cond_load_relaxed
+#define smp_cond_load_relaxed(ptr, cond_expr) ({        \
+    typeof(ptr) __PTR = (ptr);                \
+    typeof(*ptr) VAL;                    \
+    for (;;) {                        \
+        VAL = READ_ONCE(*__PTR);            \
+        if (cond_expr)                    \
+            break;                    \
+        cpu_relax();                    \
+    }                            \
+    VAL;                            \
+})
 #endif
 
 /**
@@ -227,22 +251,13 @@ do {									\
  *
  * Equivalent to using smp_load_acquire() on the condition variable but employs
  * the control dependency of the wait to reduce the barrier on many platforms.
- *
- * Due to C lacking lambda expressions we load the value of *ptr into a
- * pre-named variable @VAL to be used in @cond.
  */
 #ifndef smp_cond_load_acquire
-#define smp_cond_load_acquire(ptr, cond_expr) ({		\
-	typeof(ptr) __PTR = (ptr);				\
-	typeof(*ptr) VAL;					\
-	for (;;) {						\
-		VAL = READ_ONCE(*__PTR);			\
-		if (cond_expr)					\
-			break;					\
-		cpu_relax();					\
-	}							\
-	smp_acquire__after_ctrl_dep();				\
-	VAL;							\
+#define smp_cond_load_acquire(ptr, cond_expr) ({        \
+    typeof(*ptr) _val;                    \
+    _val = smp_cond_load_relaxed(ptr, cond_expr);        \
+    smp_acquire__after_ctrl_dep();                \
+    _val;                            \
 })
 #endif
 
